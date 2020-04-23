e70086eae8f39054a4b1961c38708e89591961dc
Cristian Anca <cristi.anca@gmail.com>
2020-04-23 23:49:56 +0300

    Use retry library Use library to be python idiomatic. Would have been much more useful if retries would be fixed and was able to use decorators, but it still looks nice.
    Signed-off-by: Cristian Anca <cristi.anca@gmail.com>

--- sdn/lib_get_data.py
+++ sdn/lib_get_data.py
@@ -6,7 +6,7 @@ import json
 import logging
 from http import HTTPStatus
 import http.client
-import time
+from retry.api import retry_call
 
 __LOGGER = logging.getLogger(__name__)
 
@@ -22,25 +22,29 @@ def get_data(url, max_retries=5, delay_between_retries=1):
     Returns:
         data (dict)
     """
-    for retry in range(max_retries + 1):
-        if retry > 0:
-            __LOGGER.info("Previous run failed, retrying - attempt %s", retry)
-            time.sleep(delay_between_retries)
-        try:
-            return __get_data_attempt(url)
-        except (
-                ValueError,                    # raised by urlopen
-                urllib.error.URLError,         # raised by urlopen
-                http.client.HTTPException,     # raised by read
-                json.decoder.JSONDecodeError   # raised by loads
-        ) as exc:
-            __LOGGER.error("Get Data failed \n%s", exc)
-            continue
-
-    return None
+    try:
+        return retry_call(
+            __get_data_attempt, fargs=[url], tries=max_retries+1, delay=delay_between_retries
+        )
+    except (
+            ValueError,                    # raised by urlopen
+            urllib.error.URLError,         # raised by urlopen
+            http.client.HTTPException,     # raised by read
+            json.decoder.JSONDecodeError   # raised by loads
+    ) as exc:
+        __LOGGER.error("Get Data failed \n%s", exc)
+        return None
 
 
 def __get_data_attempt(url):
+    """
+    Fetch the data from given URL and return it as a JSON object with no retries
+​
+    Args:
+        url (str): The url to be fetched.
+    Returns:
+        data (dict)
+    """
     with urllib.request.urlopen(url) as response:
         if response.status != HTTPStatus.OK:
             raise ValueError(f"Bad response {response.status}, {response.reason}")
