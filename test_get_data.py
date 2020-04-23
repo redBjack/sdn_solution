902ab41c3eac4cf88d51fed0cacc3acc1c7c07fa
Cristian Anca <cristi.anca@gmail.com>
2020-04-23 19:06:41 +0300

    Add test for get_data (failing)
    Signed-off-by: Cristian Anca <cristi.anca@gmail.com>

--- test_get_data.py
+++ test_get_data.py
@@ -1,7 +1,19 @@
+import pytest
 from sdn.lib_get_data import get_data
+
+__TEST_URL = "http://www.mocky.io/v2/5e539b332e00007c002dacbe"
 
 
 def test_get_data_returns_none():
     # WHEN calling get_data with a fake url
     # THEN it returns None
     assert get_data("/fake/url") is None
+
+
+@pytest.mark.xfail(reason="Not implemented yet")
+def test_get_data_returns_a_dict():
+    # WHEN calling get_data on the test url
+    response = get_data(__TEST_URL)
+
+    # THEN it returns a valid dict object
+    assert isinstance(response, dict)
