77d4d335358103198f30c72564945feeeae18ce3
Cristian Anca <cristi.anca@gmail.com>
2020-04-23 17:45:06 +0300

    Add initial code
    Signed-off-by: Cristian Anca <cristi.anca@gmail.com>

---
+++ main.py
@@ -0,0 +1,41 @@
+from data_structures.datacenter import Datacenter
+
+
+URL = "http://www.mocky.io/v2/5e539b332e00007c002dacbe"
+
+
+def get_data(url, max_retries=5, delay_between_retries=1):
+    """
+    Fetch the data from http://www.mocky.io/v2/5e539b332e00007c002dacbe
+    and return it as a JSON object.
+​
+    Args:
+        url (str): The url to be fetched.
+        max_retries (int): Number of retries.
+        delay_between_retries (int): Delay between retries in seconds.
+    Returns:
+        data (dict)
+    """
+    pass  # the rest of your logic here
+
+
+def main():
+    """
+    Main entry to our program.
+    """
+
+    data = get_data(URL)
+
+    if not data:
+        raise ValueError('No data to process')
+
+    datacenters = [
+        Datacenter(key, value)
+        for key, value in data.items()
+    ]
+
+    pass  # the rest of your logic here
+
+
+if __name__ == '__main__':
+    main()
