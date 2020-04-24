40044730c65e42fcb485d1790dca68e3e8655253
Cristian Anca <cristi.anca@gmail.com>
2020-04-24 22:12:12 +0300

    Test > operator. no implementation needed
    Signed-off-by: Cristian Anca <cristi.anca@gmail.com>

--- tests/data_structures/test_entry.py
+++ tests/data_structures/test_entry.py
@@ -87,3 +87,14 @@ def test_entry_with_smaller_ip_is_smaller():
     # WHEN comparing them
     # THEN the one with 192.168.1.1 is smaller
     assert entry1 < entry2
+
+
+def test_entry_with_bigger_ip_is_bigger():
+    # GIVEN and entry with ip 192.168.1.1
+    entry1 = Entry("192.168.1.1", True, "30/01/20 17:02:09")
+    # and one with 192.168.2.1
+    entry2 = Entry("192.168.2.1", True, "30/01/20 17:02:09")
+
+    # WHEN comparing them
+    # THEN the one with 192.168.2.1 is bigger
+    assert entry2 > entry1
