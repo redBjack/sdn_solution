62eca598a7e6f414d72b7d0e333956f4b158e70c
Cristian Anca <cristi.anca@gmail.com>
2020-04-24 20:36:52 +0300

    Extract fixture for common data
    Signed-off-by: Cristian Anca <cristi.anca@gmail.com>

--- tests/data_structures/test_network_collection.py
+++ tests/data_structures/test_network_collection.py
@@ -4,17 +4,9 @@ from data_structures.network_collection import NetworkCollection
 from data_structures.entry import Entry
 
 
-def test_network_collection_inits_network_ip():
-    # WHEN creating a NetworkCollection instance
-    network_collection = NetworkCollection("192.168.0.0/24", [])
-
-    # THEN the ipv4_network field is a valid ip field
-    assert isinstance(network_collection.ipv4_network, IPv4Network)
-
-
-def test_network_collection_init_entries():
-    # WHEN creating a NetworkCollection instance
-    network_collection = NetworkCollection(
+@pytest.fixture
+def two_entries_collection():
+    yield NetworkCollection(
         "192.168.0.0/24",
         [
             {
@@ -30,10 +22,22 @@ def test_network_collection_init_entries():
         ]
     )
 
+
+def test_network_collection_inits_network_ip():
+    # WHEN creating a NetworkCollection instance
+    network_collection = NetworkCollection("192.168.0.0/24", [])
+
+    # THEN the ipv4_network field is a valid ip field
+    assert isinstance(network_collection.ipv4_network, IPv4Network)
+
+
+def test_network_collection_init_entries(two_entries_collection):
+    # WHEN creating a NetworkCollection instance (in fixture)
+
     # THEN the entries field is properly initialized
-    assert isinstance(network_collection.entries, list)
-    assert len(network_collection.entries) == 2
-    assert all([isinstance(entry, Entry) for entry in network_collection.entries])
+    assert isinstance(two_entries_collection.entries, list)
+    assert len(two_entries_collection.entries) == 2
+    assert all([isinstance(entry, Entry) for entry in two_entries_collection.entries])
 
 
 def test_network_collection_inits_from_json_example(response_json_example):
